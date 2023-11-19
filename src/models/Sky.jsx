cc8fc419f4ba119e614767a5926d9eb87ed0e8ac
abdalla amin <abdo.faozy@gmail.com>
2023-11-19 02:22:08 +0200

    project done

---
+++ src/models/Sky.jsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+import skyScene from "../assets/3d/sky.glb";
+
+
+export function Sky({ isRotating }) {
+  const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+
+  // It ensures smooth animations by making the rotation frame rate-independent.
+  // 'delta' represents the time in seconds since the last frame.
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.25 * delta; // Adjust the rotation speed as needed
+    }
+  });
+
+  return (
+    <mesh ref={skyRef}>
+      // use the primitive element when you want to directly embed a complex 3D
+      model or scene
+      <primitive object={sky.scene} />
+    </mesh>
+  );
+}
